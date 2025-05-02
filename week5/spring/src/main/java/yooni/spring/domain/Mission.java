7713b3f20757aaa864d0a6aa2215b4cb090ff944
Yoonji Lee <estelle0329@ewha.ac.kr>
2025-05-02 12:06:34 +0900

    week5: jpa basics and initial settings

---
+++ week5/spring/src/main/java/yooni/spring/domain/Mission.java
@@ -0,0 +1,38 @@
+package yooni.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import yooni.spring.domain.common.BaseEntity;
+import yooni.spring.domain.mapping.MemberMission;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Mission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private Integer reward;
+
+    @Column(nullable = false)
+    private LocalDate deadline;
+
+    @Column(length = 255)
+    private String missionSpec;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "store_id")
+    private Store store;
+
+    @OneToMany(mappedBy = "mission", cascade = CascadeType.ALL)
+    private List<MemberMission> memberMissionList = new ArrayList<>();
+}
---
+++ week5/spring/src/main/java/yooni/spring/domain/Store.java
@@ -0,0 +1,35 @@
+package yooni.spring.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+import yooni.spring.domain.common.BaseEntity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class Store extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column(nullable = false, length = 100)
+    private String address;
+
+    @Column(length = 30)
+    private String phone;
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Mission> missionList = new ArrayList<>();
+
+    @OneToMany(mappedBy = "store", cascade = CascadeType.ALL)
+    private List<Review> reviewList = new ArrayList<>();
+}
